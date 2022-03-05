8d07a1c160bf522913cd64da9cb01aef8b2b07db
Sambit Kumar Nanda <sambitn20@gmail.com>
2022-03-05 12:51:36 +0530

    api fetch and work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useState } from "react";
+import "./App.css";
+import { createWorker } from "tesseract.js";
+import lodimg from "./image/index.svg";
 
 function App() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [textResult, setTextResult] = useState("");
+  const [loading, setLoading] = useState();
+  const worker = createWorker();
+
+  const convertImageToText = useCallback(async () => {
+    if (!selectedImage) return;
+    await worker.load();
+    await worker.loadLanguage("eng");
+    await worker.initialize("eng");
+    const { data } = await worker.recognize(selectedImage);
+    setTextResult(data.text);
+    await worker.terminate();
+    setLoading(false);
+  }, [worker, selectedImage]);
+
+  useEffect(() => {
+    convertImageToText();
+  }, [selectedImage, convertImageToText]);
+
+  const handelChangeImage = (e) => {
+    setSelectedImage(e.target.files[0]);
+    setLoading(true);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ImText</h1>
+      <p>Gets Word In Image</p>
+      <div className="input-wrapper">
+        <label htmlFor="upload">Upload Image</label>
+        <input
+          type="file"
+          id="upload"
+          accept="image/*"
+          onChange={handelChangeImage}
+        />
+      </div>
+      <div className="loading">
+        {loading && (
+          <div className="loader-wrapper">
+            <img src={lodimg} alt="img-loader" />
+            <h3>Loading.....</h3>
+          </div>
+        )}
+      </div>
+
+      <div className="result">
+        {selectedImage && (
+          <div className="box-image">
+            <img src={URL.createObjectURL(selectedImage)} alt="thumb" />
+          </div>
+        )}
+        {textResult && (
+          <div className="box-p">
+            <p>{textResult}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
